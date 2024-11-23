ab91c60cd6b4ba8f3a4b2d9e225b32cd27ad3296
Yicheng Zhang <zychelenzhang@gmail.com>
2024-11-23 11:10:41 +0100

    filters done

--- wonderland_frontend/src/components/Filters.jsx
+++ wonderland_frontend/src/components/Filters.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { SVGProps } from 'react';
 
 function Filters(props) {
-  // Dropdown values
+  // Dropdown values (getting from database later)
   const dropdownValues = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
 
   // State for all dropdowns
@@ -27,7 +27,7 @@ function Filters(props) {
     style={{ 
         justifyContent: 'center',
         alignItems: 'center',
-         width: '100%' }}>
+         width: '300px' }}>
       <label htmlFor={key}>{label}</label>
       <select
       class="block w-full px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -52,9 +52,14 @@ function Filters(props) {
 
     return (
       <div className="flex gap-4 mb-8">
-        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="3em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4h16v2.172a2 2 0 0 1-.586 1.414L15 12v7l-6 2v-8.5L4.52 7.572A2 2 0 0 1 4 6.227z"></path></svg>
-        <div class="relative inline-block text-left" style={{ display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
-      {['Dropdown 1', 'Dropdown 2', 'Dropdown 3', 'Dropdown 4'].map((label, index) =>
+        
+        <div class="relative inline-block text-left" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', gap: '20px', marginBottom: '20px' }}>
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="4em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4h16v2.172a2 2 0 0 1-.586 1.414L15 12v7l-6 2v-8.5L4.52 7.572A2 2 0 0 1 4 6.227z"></path></svg>
+        <div style={{textAlign: "left", marginRight: "50px"}}>
+  <b>Filters</b>
+</div>
+      {['Software Installation', 'Type', 'Organization Units', 'Services'].map((label, index) =>
         renderDropdown(label, `dropdown${index + 1}`)
       )}
     </div>
